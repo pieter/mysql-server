f806d3bd1ddabb7b29e976fc3525af3378f46594
none <rafal@quant.>
2008-01-09 23:31:00 +0100

    This patch adds si_object.h file.

---
+++ sql/backup/si_objects.h
@@ -0,0 +1,16 @@
+#ifndef SI_OBJECTS_H_
+#define SI_OBJECTS_H_
+
+/**
+ @file
+ 
+ This file defines the API for the following object services:
+ 
+ - enumerating objects,
+ - getting CREATE statements for different types of objects,
+ - finding dependencies between objects,
+
+ */ 
+
+
+#endif /*SI_OBJECTS_H_*/

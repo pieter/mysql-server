3e7b608de8725731c6a930101937f360cbd3c948
none <rafal@quant.>
2008-01-11 22:42:45 +0100

    WL#3574 : Online backup: Service API for Metadata Objects II
    Enumerators for databases, tables and views.

--- sql/backup/si_objects.cc
+++ sql/backup/si_objects.cc
@@ -17,91 +17,10 @@
 
 #include "si_objects.h"
 
-//
-// Implementation: iterator impl classes.
-//
+TABLE *create_schema_table(THD *thd, TABLE_LIST *table_list); // defined in sql_show.cc
+static TABLE* open_schema_table(THD *thd, ST_SCHEMA_TABLE *st);
 
-///////////////////////////////////////////////////////////////////////////
-
-#if 0
-class DatabaseListIterator : public ObjIterator
-{
-public:
-  DatabaseIterator();
-
-public:
-  virtual DatabaseObj *next();
-};
-
-class DbTablesIterator : public ObjIterator
-{
-public:
-  DbTablesIterator(const LEX_STRING db_name);
-
-public:
-  virtual TableObj *next();
-};
-
-class DbViewIterator : public ObjIterator
-{
-public:
-  DbViewIterator(const LEX_STRING db_name);
-
-public:
-  virtual TableObj *next();
-}
-
-class DbTriggerIterator : public ObjIterator
-{
-public:
-  DbTriggerIterator(const LEX_STRING db_name);
-
-publbic:
-  virtual TriggerObj *next();
-};
-
-class DbStoredProcIterator : public ObjIterator
-{
-public:
-  DbStoredProcIterator(const LEX_STRING db_name);
-
-public:
-  virtual StoredProcObj *next();
-};
-
-class DbStoredFuncIterator : public ObjIterator
-{
-public:
-  DbStoredFuncIterator(const LEX_STRING db_name);
-
-public:
-  virtual StoredFuncObj *next();
-}
-
-class DbEventIterator : public ObjIterator
-{
-public:
-  DbEventbIterator(const LEX_STRING db_name);
-
-public:
-  virtual EventObj *next();
-};
-
-///////////////////////////////////////////////////////////////////////////
-
-//
-// Implementation: DatabaseIterator class.
-//
-
-///////////////////////////////////////////////////////////////////////////
-
-ObjIterator *get_databases()
-{
-  return new DatabaseIterator();
-}
-
-///////////////////////////////////////////////////////////////////////////
-
+namespace obs {
 //
 // Implementation: object impl classes.
 //
@@ -111,7 +30,7 @@ ObjIterator *get_databases()
 class DatabaseObj : public Obj
 {
 public:
-  DatabaseObj(const LEX_STRING db_name);
+  DatabaseObj(const String *db_name);
 
 public:
   virtual bool serialize(THD *thd, String *serialialization);
@@ -120,10 +39,13 @@ public:
                           const String *serialialization);
 
   virtual bool execute();
+  
+  const String* get_name()
+  { return &m_db_name; } 
 
 private:
   // These attributes are to be used only for serialialization.
-  LEX_STRING m_db_name;
+  String m_db_name;
 
 private:
   // These attributes are to be used only for materialization.
@@ -135,8 +57,8 @@ private:
 class TableObj : public Obj
 {
 public:
-  TableObj(const LEX_STRING db_name,
-           const LEX_STRING table_name,
+  TableObj(const String *db_name,
+           const String *table_name,
            bool table_is_view);
 
 public:
@@ -147,10 +69,13 @@ public:
 
   virtual bool execute();
 
+  const String* get_name()
+  { return &m_table_name; } 
+
 private:
   // These attributes are to be used only for serialialization.
-  LEX_STRING m_db_name;
-  LEX_STRING m_table_name;
+  String m_db_name;
+  String m_table_name;
   bool m_table_is_view;
 
 private:
@@ -163,6 +88,7 @@ private:
 };
 
 ///////////////////////////////////////////////////////////////////////////
+
 
 class TriggerObj : public Obj
 {
@@ -269,70 +195,267 @@ private:
 ///////////////////////////////////////////////////////////////////////////
 
 //
+// Implementation: iterator impl classes.
+//
+
+///////////////////////////////////////////////////////////////////////////
+
+class DatabaseIterator : public ObjIterator
+{
+public:
+  DatabaseIterator(THD *thd);
+  ~DatabaseIterator();
+
+public:
+  virtual DatabaseObj *next();
+  
+private:
+
+  THD    *m_thd;
+  TABLE  *is_schemata;
+  handler *ha;
+  my_bitmap_map *old_map;
+
+  friend ObjIterator* get_databases(THD*);
+};
+
+class DbTablesIterator : public ObjIterator
+{
+public:
+  DbTablesIterator(THD *thd, const String *db_name);
+  ~DbTablesIterator();
+
+public:
+  virtual TableObj *next();
+
+private:
+
+  THD   *m_thd;
+  String m_db_name;
+  TABLE *is_tables;
+  handler *ha;
+  my_bitmap_map *old_map;
+  
+protected:
+
+  bool enumerate_views;
+  
+  friend ObjIterator *get_db_tables(THD*, const String*);
+};
+
+class DbViewsIterator : public DbTablesIterator
+{
+public:
+  DbViewsIterator(THD *thd, const String *db_name): DbTablesIterator(thd,db_name)
+  { enumerate_views= TRUE; }
+
+  friend ObjIterator *get_db_views(THD*, const String*);
+};
+
+
+//
+// Implementation: DatabaseIterator class.
+//
+
+///////////////////////////////////////////////////////////////////////////
+
+ObjIterator *get_databases(THD *thd)
+{
+  DatabaseIterator *it= new DatabaseIterator(thd);
+
+  return it && it->is_valid ? it : NULL;
+}
+
+DatabaseIterator::DatabaseIterator(THD *thd):
+  m_thd(thd), is_schemata(NULL)
+{
+  DBUG_ASSERT(m_thd);
+  
+  is_schemata = open_schema_table(m_thd, get_schema_table(SCH_SCHEMATA));
+
+  if (!is_schemata)
+    return;
+
+  ha = is_schemata->file;
+
+  if (!ha)
+    return;
+
+  old_map= dbug_tmp_use_all_columns(is_schemata, is_schemata->read_set);
+
+  if (ha->ha_rnd_init(TRUE))
+  {
+    ha= NULL;
+    return;
+  }
+  
+  is_valid= TRUE;
+}
+
+DatabaseIterator::~DatabaseIterator()
+{
+  if (ha)
+   ha->ha_rnd_end();
+
+  DBUG_ASSERT(m_thd);
+  
+  if (is_schemata)
+  {
+    dbug_tmp_restore_column_map(is_schemata->read_set, old_map);
+    free_tmp_table(m_thd, is_schemata);
+  }
+}
+
+DatabaseObj* DatabaseIterator::next()
+{
+  if (!is_valid)
+    return NULL;
+    
+  DBUG_ASSERT(ha);
+  DBUG_ASSERT(is_schemata);
+  
+  if (ha->rnd_next(is_schemata->record[0]))
+    return NULL;
+    
+  String name;
+
+  is_schemata->field[1]->val_str(&name);
+
+  DBUG_PRINT("DatabaseIterator::next", (" Found database %s", name.ptr()));
+
+  return new DatabaseObj(&name);
+}
+
+//
+// Implementation: DbTablesIterator class.
+//
+
+///////////////////////////////////////////////////////////////////////////
+
+ObjIterator *get_db_tables(THD *thd, const String *db_name)
+{
+  DbTablesIterator *it= new DbTablesIterator(thd, db_name);
+
+  return it && it->is_valid ? it : NULL;
+}
+
+ObjIterator *get_db_views(THD *thd, const String *db_name)
+{
+  DbViewsIterator *it= new DbViewsIterator(thd, db_name);
+
+  return it && it->is_valid ? it : NULL;
+}
+
+DbTablesIterator::DbTablesIterator(THD *thd, const String *db_name):
+  m_thd(thd), m_db_name(*db_name), 
+  is_tables(NULL), ha(NULL), old_map(NULL),
+  enumerate_views(FALSE)
+{
+  DBUG_ASSERT(m_thd);
+  m_db_name.copy();
+  
+  is_tables = open_schema_table(m_thd, get_schema_table(SCH_TABLES));
+
+  if (!is_tables)
+    return;
+
+  ha = is_tables->file;
+
+  if (!ha)
+    return;
+
+  old_map= dbug_tmp_use_all_columns(is_tables, is_tables->read_set);
+
+  if (ha->ha_rnd_init(TRUE))
+  {
+    ha= NULL;
+    return;
+  }
+  
+  is_valid= TRUE;
+}
+
+DbTablesIterator::~DbTablesIterator()
+{
+  if (ha)
+   ha->ha_rnd_end();
+
+  DBUG_ASSERT(m_thd);
+  
+  if (is_tables)
+  {
+    dbug_tmp_restore_column_map(is_tables->read_set, old_map);
+    free_tmp_table(m_thd, is_tables);
+  }
+}
+
+TableObj* DbTablesIterator::next()
+{
+  if (!is_valid)
+    return NULL;
+    
+  DBUG_ASSERT(ha);
+  DBUG_ASSERT(is_tables);
+  
+  while (!ha->rnd_next(is_tables->record[0]))
+  {
+    String table_name;
+    String db_name;
+    String type;
+
+    is_tables->field[1]->val_str(&db_name);
+    is_tables->field[2]->val_str(&table_name);
+    is_tables->field[3]->val_str(&type);
+
+    // skip tables not from the given database
+    if (db_name != m_db_name)
+      continue;
+      
+    // skip tables/views depending on enumerate_views flag
+    if (type != ( enumerate_views ? String("VIEW", system_charset_info) :
+                                    String("BASE TABLE", system_charset_info)) )
+      continue;
+
+    DBUG_PRINT("DbTablesIterator::next", (" Found table %s.%s", 
+                                          db_name.ptr(), table_name.ptr()));
+    return new TableObj(&db_name,&table_name,enumerate_views);
+  }
+  
+  return NULL;
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+//
 // Implementation: DatabaseObj class.
 //
 
 ///////////////////////////////////////////////////////////////////////////
 
-DatabaseObj::DatabaseObj(const LEX_STRING db_name) :
+DatabaseObj::DatabaseObj(const String *db_name) :
   m_db_name(*db_name)
 {
-  // TODO: return an error for pseudo-databases|tables or "mysql".
+  m_db_name.copy(); // copy name string to newly allocated memory
 }
 
 bool DatabaseObj::serialize(THD *thd, String *serialialization)
 {
-  // XXX: this is a copy & paste of mysql_show_create_database().
-
-  HA_CREATE_INFO create;
-
-// debug  if (!my_strcasecmp(system_charset_info, m_db_name.str,
-// debug                     INFORMATION_SCHEMA_NAME.str))
-// debug  {
-// debug    m_db_name.str= INFORMATION_SCHEMA_NAME.str;
-// debug    create.default_table_charset= system_charset_info;
-// debug  }
-// debug  else
-
-    if (check_db_dir_existence(m_db_name.str))
-    {
-      my_error(ER_BAD_DB_ERROR, MYF(0), m_db_name.str);
-      return -1;
-    }
-
-    load_db_opt_by_name(thd, m_db_name.str, &create);
-
-  serialialization->append(STRING_WITH_LEN("CREATE DATABASE "));
-  append_identifier(thd, serialialization, m_db_name.str, m_db_name.length);
-
-  if (create.default_table_charset)
-  {
-    serialialization->append(STRING_WITH_LEN(" DEFAULT CHARACTER SET "));
-    serialialization->append(create.default_table_charset->csname);
-    if (!(create.default_table_charset->state & MY_CS_PRIMARY))
-    {
-      serialialization->append(STRING_WITH_LEN(" COLLATE "));
-      serialialization->append(create.default_table_charset->name);
-    }
-  }
-
-  return 0;
+  return FALSE;
 }
 
 bool DatabaseObj::materialize(uint serialization_version,
                              const String *serialialization)
 {
-  // XXX: take serialization_version into account.
-
-  m_create_stmt= *serialialization;
+  return FALSE;
 }
 
 bool DatabaseObj::execute()
 {
-  // TODO.
+  return FALSE;
 }
 
 ///////////////////////////////////////////////////////////////////////////
+
 
 //
 // Implementation: TableObj class.
@@ -340,30 +463,20 @@ bool DatabaseObj::execute()
 
 ///////////////////////////////////////////////////////////////////////////
 
-TableObj::TableObj(const LEX_STRING db_name.
-                   const LEX_STRING table_name,
+TableObj::TableObj(const String *db_name,
+                   const String *table_name,
                    bool table_is_view) :
   m_db_name(*db_name),
   m_table_name(*table_name),
   m_table_is_view(table_is_view)
 {
-  // TODO: return an error for pseudo-databases|tables or "mysql".
+  m_db_name.copy();
+  m_table_name.copy();
 }
 
 bool TableObj::serialize(THD *thd, String *serialialization)
 {
-  // XXX: this is a copy & paste from mysqld_show_create() and
-  // store_create_info().
-
-  // NOTE: we don't care about 1) privilege checking; 2) meta-data locking
-  // here.
-
-  // XXX: we don't add version-specific comment here. materialize()
-  // function should know how to materialize the information.
-
-  return m_table_is_view ?
-         serialize_table(thd, serialialization) :
-         serialize_view(thd, serialialization);
+  return FALSE;
 }
 
 bool TableObj::serialize_table(THD *thd, String *serialialization)
@@ -379,15 +492,110 @@ bool TableObj::serialize_view(THD *thd, String *serialialization)
 bool TableObj::materialize(uint serialization_version,
                           const String *serialialization)
 {
-  // XXX: take serialization_version into account.
-
-  m_create_stmt= *serialialization;
+  return 0;
 }
 
 bool TableObj::execute()
 {
-  // TODO.
+  return 0;
 }
+
+
+#if 0
+
+
+class DbTriggerIterator : public ObjIterator
+{
+public:
+  DbTriggerIterator(const LEX_STRING db_name);
+
+publbic:
+  virtual TriggerObj *next();
+};
+
+class DbStoredProcIterator : public ObjIterator
+{
+public:
+  DbStoredProcIterator(const LEX_STRING db_name);
+
+public:
+  virtual StoredProcObj *next();
+};
+
+class DbStoredFuncIterator : public ObjIterator
+{
+public:
+  DbStoredFuncIterator(const LEX_STRING db_name);
+
+public:
+  virtual StoredFuncObj *next();
+};
+
+class DbEventIterator : public ObjIterator
+{
+public:
+  DbEventbIterator(const LEX_STRING db_name);
+
+public:
+  virtual EventObj *next();
+};
+
+///////////////////////////////////////////////////////////////////////////
+
+
+
 #endif
+
+} // obs namespace
+
+
+/// Open given table in @c INFORMATION_SCHEMA database.
+TABLE* open_schema_table(THD *thd, ST_SCHEMA_TABLE *st)
+{
+  TABLE *t;
+  TABLE_LIST arg;
+  my_bitmap_map *old_map;
+
+  bzero( &arg, sizeof(TABLE_LIST) );
+
+  // set context for create_schema_table call
+  arg.schema_table= st;
+  arg.alias=        NULL;
+  arg.select_lex=   NULL;
+
+  t= create_schema_table(thd,&arg); // Note: callers must free t.
+
+  if( !t ) return NULL; // error!
+
+  /*
+   Temporarily set thd->lex->wild to NULL to keep st->fill_table
+   happy.
+  */
+  ::String *wild= thd->lex->wild;
+  ::enum_sql_command command= thd->lex->sql_command;
+
+  thd->lex->wild = NULL;
+  thd->lex->sql_command = enum_sql_command(0);
+
+  // context for fill_table
+  arg.table= t;
+
+  old_map= tmp_use_all_columns(t, t->read_set);
+
+  /*
+    Question: is it correct to fill I_S table each time we use it or should it
+    be filled only once?
+   */
+  st->fill_table(thd,&arg,NULL);  // NULL = no select condition
+
+  tmp_restore_column_map(t->read_set, old_map);
+
+  // undo changes to thd->lex
+  thd->lex->wild= wild;
+  thd->lex->sql_command= command;
+
+  return t;
+}
+
 
 ///////////////////////////////////////////////////////////////////////////
--- sql/backup/si_objects.h
+++ sql/backup/si_objects.h
@@ -24,6 +24,8 @@ public:
                           const String *serialialization) = 0;
 
   virtual bool execute() = 0;
+  
+  virtual const String* get_name() = 0;
 
 public:
   virtual ~Obj()
@@ -34,7 +36,13 @@ public:
 
 class ObjIterator
 {
+protected:
+  bool is_valid;  ///< Indicates if iterator has been constructed successfully.
 public:
+
+  ObjIterator(): is_valid(FALSE)
+  { }
+
   virtual Obj *next() = 0;
   // User is responsible for destroying the returned object.
 
@@ -63,9 +71,9 @@ Obj *get_event(const LEX_STRING db_name, const LEX_STRING event_name);
 
 // User is responsible for destroying the returned iterator.
 
-ObjIterator *get_databases();
-ObjIterator *get_db_tables(const LEX_STRING db_name);
-ObjIterator *get_db_views(const LEX_STRING db_name);
+ObjIterator *get_databases(THD *thd);
+ObjIterator *get_db_tables(THD *thd, const String *db_name);
+ObjIterator *get_db_views(THD *thd, const String *db_name);
 ObjIterator *get_db_triggers(const LEX_STRING db_name);
 ObjIterator *get_db_stored_procedures(const LEX_STRING db_name);
 ObjIterator *get_db_stored_functions(const LEX_STRING db_name);
@@ -98,6 +106,6 @@ Obj *create_object(ObjectType object_type,
 
 ///////////////////////////////////////////////////////////////////////////
 
-}
+} // obs namespace
 
 #endif // SI_OBJECTS_H_

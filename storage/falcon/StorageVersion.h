a52fa139c152adb0fa25ee947d9d7856da2fb257
none <hakank/hakan@lu0011.>
2008-04-01 02:37:14 +0200

    Bumped internal Falcon version.

--- storage/falcon/StorageVersion.h
+++ storage/falcon/StorageVersion.h
@@ -14,5 +14,5 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 
-#define FALCON_VERSION	"T1.1-3"
-#define FALCON_DATE		"30 March, 2008"
+#define FALCON_VERSION	"T1.1-4"
+#define FALCON_DATE		"01 April, 2008"
